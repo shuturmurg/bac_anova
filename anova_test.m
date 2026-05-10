f8ce9d56e30bddddd48bceda7ce48f737ef75f1c
arjun <arjunchakravarty11@gmail.com>
2026-05-11 00:23:43 +0530

    Update anova_test.m

--- anova_test.m
+++ anova_test.m
@@ -1,5 +1,5 @@
 count = [2.30e7; 1.75e7; 8.00e6; 1.65e7; 1.43e7; 2.00e7];
-ph = [2; 3; 4; 4; 7; 7];
+ph = [2; 2; 4; 4; 7; 7];
 tryp = [false; true; false; true; false; true];
 
 tryp_cat = categorical(tryp, [false, true], {'No Trypsin,', 'Trypsin present'});
